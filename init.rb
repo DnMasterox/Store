ce865d3394bb068343424703d69e0a9a9d6c381a
mshumakov <mastershumakov@gmail.com>
2018-12-05 14:16:21 +0200

    Send method

--- init.rb
+++ init.rb
@@ -17,5 +17,20 @@ cart = Cart.new('mykola')
 cart.add_item RealItem.new(price: 100, weight: 101, name: 'car')
 cart.add_item RealItem.new(price: 100, weight: 200, name: 'car')
 cart.add_item RealItem.new(price: 100, weight: 120, name: 'kettle')
-puts cart.all_cars
-puts cart.all_kettles
+
+# puts cart.all_cars
+# puts cart.all_kettlesß
+
+puts cart.kind_of?(Cart)
+puts @items[0].kind_of?(Item)
+puts @items[0].kind_of?(AntiqueItem)
+puts @items[0].class == AntiqueItem
+puts @items[0].class == Item
+puts @items[0].respond_to?(:info)
+
+puts @items[0].send(:price) #can call methods and private methods
+puts @items[0].send(:tax)
+puts @items[0].price
+
+method_one = 'all_cars'
+puts cart.send(method_one)
