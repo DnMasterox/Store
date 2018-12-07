9b8fe86d8ea3f41a6402661a040df717b8583688
mshumakov <mastershumakov@gmail.com>
2018-12-07 15:12:26 +0200

    Email notifications

--- order.rb
+++ order.rb
@@ -11,23 +11,29 @@ class Order
   def read_credentials_from_file
     @credentials = Array.new
     File.readlines('credentials.txt').each {|i| @credentials << i.to_s}
-    @credentials.each(&:chomp) # "car:100:50"
+    @credentials.each(&:chomp)
     @credentials
   end
 
   def place
     login = read_credentials_from_file[1]
     password = read_credentials_from_file[0]
-    Pony.mail(to: StoreApplication::Admin.email,
-              from: "My Store <#{login}>",
-              via: :smtp, via_options: {
-                address: 'smtp.gmail.com',
-                port: '587',
-                user_name: login.to_s,
-                password: password.to_s,
-                authentication: :plain,
-                domain: 'mail.google.com'},
-              subject: 'New order has been placed', body: 'Please check back you 
+    thread = Thread.new do # look to the delayed_job gem
+      Pony.mail(to: StoreApplication::Admin.email,
+                from: "My Store <#{login}>",
+                via: :smtp, via_options: {
+              address: 'smtp.gmail.com',
+              port: '587',
+              user_name: login.to_s,
+              password: password.to_s,
+              authentication: :plain,
+              domain: 'mail.google.com'},
+                subject: 'New order has been placed', body: 'Please check back you
 admin page to see it!')
+    end
+    while thread.alive?
+      puts '.'
+      sleep 1
+    end
   end
 end
